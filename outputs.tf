d62c483155ccb8dd1ed35989f09150b3852d91ea
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-08-24 20:52:24 -0500

    docs: expose route and NAT outputs for integrations

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,21 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
+output "public_route_table_id" {
+  description = "Public route table ID"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "Private route table IDs"
+  value       = aws_route_table.private[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "NAT gateway IDs"
+  value       = aws_nat_gateway.main[*].id
+}
+
 output "web_security_group_id" {
   description = "Web security group ID"
   value       = aws_security_group.web.id
